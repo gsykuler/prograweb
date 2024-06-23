d5cf835512dec8b9f08728ba81826c46ff073e1a
gsykuler <148912299+gsykuler@users.noreply.github.com>
2024-06-23 17:49:31 -0300

    listo

--- app.js
+++
@@ -1,9 +0,0 @@
-// Seleccionar todos los elementos con la clase 'faq'
-const faqs = document.querySelectorAll(".faq"); // Usar querySelectorAll para seleccionar múltiples elementos
-
-// Agregar un evento de clic a cada elemento 'faq'
-faqs.forEach((faq) => {
-  faq.addEventListener("click", () => {
-    faq.classList.toggle("active"); // Alternar la clase 'active'
-  });
-});
--- script.js
+++ script.js
@@ -211,3 +211,13 @@ var confetti = {
 		}
 	}
 })();
+
+// Seleccionar todos los elementos con la clase 'faq'
+const faqs = document.querySelectorAll(".faq"); // Usar querySelectorAll para seleccionar múltiples elementos
+
+// Agregar un evento de clic a cada elemento 'faq'
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active"); // Alternar la clase 'active'
+  });
+});
